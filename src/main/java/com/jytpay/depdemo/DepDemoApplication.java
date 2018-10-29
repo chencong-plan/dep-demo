844dc73961f34e440a403ac147d91ce8b62590af
chencong-plan <1042738887@qq.com>
2018-10-29 18:39:27 +0800

    init project

---
+++ src/main/java/com/jytpay/depdemo/DepDemoApplication.java
@@ -0,0 +1,12 @@
+package com.jytpay.depdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DepDemoApplication.class, args);
+    }
+}
