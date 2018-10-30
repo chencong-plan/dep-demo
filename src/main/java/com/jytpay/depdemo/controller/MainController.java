d6dc534c28ddc4f67139f80f91016242c9f12dc5
chencong-plan <1042738887@qq.com>
2018-10-30 13:08:47 +0800

    添加static资源

--- src/main/java/com/jytpay/depdemo/controller/MainController.java
+++ src/main/java/com/jytpay/depdemo/controller/MainController.java
@@ -1,14 +1,34 @@
 package com.jytpay.depdemo.controller;
 
+import com.alibaba.fastjson.JSON;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class MainController {
 
 
     @RequestMapping("/test")
-    public String index(){
+    public String index(Model model){
+        List<String> list = new ArrayList<>();
+        list.add("chencong");
+        list.add("22");
+        Map<String,String> map = new HashMap<>();
+        map.put("amount","12.1");
+        map.put("list",list.toString());
+        String result  = JSON.toJSONString(map,true);
+        model.addAttribute("result",result);
         return "test";
+    }
+
+    @RequestMapping("/index")
+    public String home(){
+        return "index";
     }
 }
