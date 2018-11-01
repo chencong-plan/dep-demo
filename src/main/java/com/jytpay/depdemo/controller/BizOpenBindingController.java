916396336a04a798376e31550d4f1b080bd97913
chencong-plan <1042738887@qq.com>
2018-11-01 20:44:37 +0800

    对开户绑卡页面进行修改

--- src/main/java/com/jytpay/depdemo/controller/BizOpenBindingController.java
+++ src/main/java/com/jytpay/depdemo/controller/BizOpenBindingController.java
@@ -1,14 +1,11 @@
 package com.jytpay.depdemo.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.jytpay.depdemo.Util.GenerateSequenceUtil;
 import com.jytpay.depdemo.service.OpenAndBindCardService;
 import com.jytpay.depdemo.vo.BaseJsonReqVo;
 import com.jytpay.depdemo.vo.Result;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
