907ef7f3725515c0c70054ab8dfaa2e5a732d7e6
chencong-plan <1042738887@qq.com>
2018-12-07 17:39:32 +0800

    字段校验注解

--- src/main/java/com/jytpay/depdemo/annotation/Validate.java
+++ src/main/java/com/jytpay/depdemo/annotation/Validate.java
@@ -2,12 +2,15 @@ package com.jytpay.depdemo.annotation;
 
 import java.lang.annotation.*;
 
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+
 /**
  * @author www.ccoder.cc
  */
 @Documented
 @Inherited
-@Target({ElementType.FIELD, ElementType.METHOD})
+@Target({FIELD, METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Validate {
 
